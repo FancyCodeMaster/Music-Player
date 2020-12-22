dad1febbe536719f686f02af7e3aee3204785c08
FancyCodeMaster <fancycodemaster@gmail.com>
2020-12-23 04:13:05 +0545

    first commit

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const initialTime = document.getElementById("initial-time");
 const totalTime = document.getElementById("total-time");
+const progressBarContainer = document.getElementById("progress-bar-container");
+const progress = document.getElementById("progress");
 
 let isPlaying = false;
 let songIndex = 0;
+let isShuffle = false;
 
 // For Previous Button and Play Button
 const Songs = [
@@ -47,6 +50,9 @@ const play = () => {
     music.play();
     playBtn.classList.replace("fa-play" , "fa-pause");
     playBtn.title = "Pause";
+
+    // Making Image Rotate
+    titleImage.classList.add("rotate");
 }
 
 
@@ -56,6 +62,9 @@ const pause = () => {
     music.pause();
     playBtn.classList.replace("fa-pause" , "fa-play");
     playBtn.title = "Play";
+
+    // Stopping Image to rotate
+    titleImage.classList.remove("rotate");
 }
 
 
@@ -86,7 +95,12 @@ playBtn.addEventListener("click" , () => {
 // For that we used array on the top of this js page.
 
 const prevSong = () => {
-    if(songIndex<=3 && songIndex>0) {
+    if(isShuffle===true){
+        let checkIndex = Math.floor(Math.random() * 3);
+        songIndex = checkIndex;
+        console.log(songIndex);
+    }
+    else if((songIndex<=3 && songIndex>0) && isShuffle===false) {
         songIndex--;
     }
     else {
@@ -100,8 +114,10 @@ const prevSong = () => {
 
     // Play the song immediately after all the sources , textContent have been changed
     music.play();
+    isPlaying = true;
     playBtn.classList.replace("fa-play" , "fa-pause");
     playBtn.title = "Pause";
+    titleImage.classList.add("rotate");
 };
 
 prevBtn.addEventListener("click" , prevSong)
@@ -122,7 +138,16 @@ prevBtn.addEventListener("click" , prevSong)
 // For that we used array on the top of this js page.
 
 const nextSong = () => {
-    if(songIndex>=0 && songIndex<(Songs.length-1)) {
+    if(isShuffle===true) {
+        let checkIndex = Math.floor(Math.random() * 3);
+        if(checkIndex !== songIndex) {
+            songIndex = checkIndex;
+        }
+        else {
+            songIndex++;
+        }
+    }
+    else if((songIndex>=0 && songIndex<(Songs.length-1)) && isShuffle===false) {
         songIndex++;
     }
     else {
@@ -136,8 +161,10 @@ const nextSong = () => {
 
     // Play the song immediately after all the sources , textContent have been changed
     music.play();
+    isPlaying = true;
     playBtn.classList.replace("fa-play" , "fa-pause");
     playBtn.title = "Pause";
+    titleImage.classList.add("rotate");
 };
 
 nextBtn.addEventListener("click" , nextSong)
@@ -158,6 +185,7 @@ nextBtn.addEventListener("click" , nextSong)
 // When one song ends , automatically play another song
 music.addEventListener("ended" , nextSong);
 
+
 /*-------------------------------*/
 /*--------------------------------------*/
 /*--------------------------------*/
@@ -165,7 +193,7 @@ music.addEventListener("ended" , nextSong);
 
 
 /*-------------------------------*/
-/*------When We click the repeat Button-------------*/
+/*------ Repeat Button-------------*/
 /*--------------------------------------*/
 
 // repeat the same song
@@ -176,8 +204,26 @@ repeatBtn.addEventListener("click" , () => {
         music.loop = true;
     }
     else{
-        music.loop = true;
+        music.loop = false;
     }
+});
+
+/*-------------------------------*/
+/*--------------------------------------*/
+/*--------------------------------*/
+
+
+/*-------------------------------*/
+/*------Shuffle Button-------------*/
+/*--------------------------------------*/
+
+shuffleBtn.addEventListener("click" , () => {
+
+    shuffleBtn.classList.toggle("active");
+
+    (isShuffle===false)?(isShuffle = true):(isShuffle = false);
+
+
 });
 
 /*-------------------------------*/
@@ -194,32 +240,68 @@ repeatBtn.addEventListener("click" , () => {
 const getMusicTime = (event) => {
 
     let currentTime = event.srcElement.currentTime;
-    let currentMin = Math.round(currentTime / 60);
-    let currentSec = Math.round(currentTime % 60);
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
 
     let duration = event.srcElement.duration;
-    let totalMin = Math.round(duration / 60);
-    let totalSec = Math.round(duration % 60);
 
-    if(currentSec < 10) {
-        initialTime.textContent = `${currentMin}:0${currentSec}`;
-    }
-    else {
-        initialTime.textContent = `${currentMin}:${currentSec}`;
+    if(duration) {
+        let totalMin = Math.floor(duration / 60);
+        let totalSec = Math.round(duration % 60);
+    
+
+        progress.style.width = `${(currentTime / duration) * 100}%`;
+
+        if(currentSec < 10) {
+            initialTime.textContent = `${currentMin}:0${currentSec}`;
+        }
+        else {
+            initialTime.textContent = `${currentMin}:${currentSec}`;
+        }
+
+        if(totalSec < 10) {
+            totalTime.textContent = `${totalMin}:0${totalSec}`;
+        }
+        else {
+            totalTime.textContent = `${totalMin}:${totalSec}`;
+        }
+
     }
 
-    if(totalSec < 10) {
-        totalTime.textContent = `${totalMin}:0${totalSec}`;
-    }
-    else {
-        totalTime.textContent = `${totalMin}:${totalSec}`;
-    }
 
 };
 
 
 
 music.addEventListener("timeupdate" , getMusicTime);
+
+/*-------------------------------*/
+/*--------------------------------------*/
+/*--------------------------------*/
+
+
+
+/*-------------------------------*/
+/*------Progress Bar-------------*/
+/*--------------------------------------*/
+
+// updateProgress Function
+const updateProgress = (event) => {
+
+    const totalWidth = Math.round(event.target.clientWidth);
+    const clickedWidth = Math.round(event.offsetX);
+
+    // getting duration of music as music(audio) has duration as an object
+    const {duration} = music;
+
+    const setWidth = (clickedWidth / totalWidth) * 100;
+    const setTime = (clickedWidth / totalWidth) * duration;
+    progress.style.width = `${setWidth}%`;
+    music.currentTime = setTime;
+
+};
+
+progressBarContainer.addEventListener("click" , updateProgress);
 
 /*-------------------------------*/
 /*--------------------------------------*/
